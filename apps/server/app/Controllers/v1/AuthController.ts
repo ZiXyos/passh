fe9531a560de4fd6f11ee35ac8e9f414fdbe199f
ZiXyos <killian.moudery@gmail.com>
2023-09-04 12:55:40 +0200

    feat(auth): add Pre hash on register

--- apps/server/app/Controllers/v1/AuthController.ts
+++ apps/server/app/Controllers/v1/AuthController.ts
@@ -2,6 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { prisma } from '@ioc:Adonis/Addons/Prisma';
 import { type LocalCredentials } from '@prisma/client';
 import { authenticatedUserResSchema, signupReqSchema } from '@passh/shared';
+import  Hash from '@ioc:Adonis/Core/Hash';
 
 export default class AuthController {
 
@@ -17,7 +18,7 @@ export default class AuthController {
     try {
       localCredential = await prisma.localCredentials.create({ data: {
         email: email, 
-        password: password,
+        password: await Hash.make(password),
         account: { create: {} }
       }});
     } catch(err) {
