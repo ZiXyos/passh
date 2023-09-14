56eecaf541a687dd648b2c72c3103f5886d5332f
ZiXyos <killian.moudery@gmail.com>
2023-09-15 00:32:32 +0200

    chore: local proxy init

---
+++ init-dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+mkcert -install
+mkcert "pass-ass.local" "*.pass-ass.local"
+mkdir -p docker/certs
+cp pass-ass.local+1-key.pem pass-ass.local+1.pem docker/certs
+rm -rf pass-ass.local+1*
