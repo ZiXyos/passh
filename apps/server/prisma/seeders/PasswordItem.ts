9caf7c562b46e98d87122871e2200c61f7ba635c
ZiXyos <killian.moudery@gmail.com>
2023-09-14 22:58:41 +0200

    delete: remove prisma for LUCID (auth error)

--- apps/server/prisma/seeders/PasswordItem.ts
+++
@@ -1,9 +0,0 @@
-import { prisma, PrismaSeederBase } from '@ioc:Adonis/Addons/Prisma'
-
-export default class PasswordItemSeeder extends PrismaSeederBase {
-  public static developmentOnly = false
-
-  public async run() {
-    // Write your database queries inside the run method
-  }
-}
