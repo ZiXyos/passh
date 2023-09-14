d32b8d90c35a3a1e74691e50a83c0a024f866d77
ZiXyos <killian.moudery@gmail.com>
2023-09-14 23:34:13 +0200

    chore: add dev creation command

--- apps/api/commands/CreateDev.ts
+++ apps/api/commands/CreateDev.ts
@@ -4,30 +4,54 @@ export default class CreateDev extends BaseCommand {
   /**
    * Command name is used to run the command
    */
-  public static commandName = 'create:dev'
+  public static commandName = 'dev:create'
 
   /**
    * Command description is displayed in the "help" output
    */
-  public static description = ''
+  public static description = 'Create a new Developper User'
 
   public static settings = {
     /**
      * Set the following value to true, if you want to load the application
-     * before running the command. Don't forget to call `node ace generate:manifest` 
+     * before running the command. Don't forget to call `node ace generate:manifest`
      * afterwards.
      */
-    loadApp: false,
+    loadApp: true,
 
     /**
      * Set the following value to true, if you want this command to keep running until
-     * you manually decide to exit the process. Don't forget to call 
+     * you manually decide to exit the process. Don't forget to call
      * `node ace generate:manifest` afterwards.
      */
     stayAlive: false,
   }
 
   public async run() {
-    this.logger.info('Hello world!')
+    const { default: LocalCredential } = await import('App/Models/LocalCredential')
+    const { default: User } = await import('App/Models/User')
+
+    this.logger.info('🔥 New Developper creation!')
+
+    const email = await this.prompt.ask('Enter email')
+    const password = await this.prompt.secure('Choose a password')
+
+    const username = await this.prompt.ask('Choose an username')
+
+    this.logger.info(' Loading...')
+
+    const localCredential = await LocalCredential.create({
+      email,
+      password,
+    })
+
+    await User.create({
+      username,
+      role: 'DEV',
+      plan: 'PREMIUM',
+      localCredentialId: localCredential.id,
+    })
+
+    this.logger.success('✅ Finished!')
   }
 }
--- apps/api/providers/AppProvider.ts
+++ apps/api/providers/AppProvider.ts
@@ -1,10 +1,20 @@
 import type { ApplicationContract } from '@ioc:Adonis/Core/Application'
+// import LocalCredential from 'App/Models/LocalCredential'
+// import Password from 'App/Models/Password'
+// import PasswordCredential from 'App/Models/PasswordCredential'
+// import User from 'App/Models/User'
 
 export default class AppProvider {
   constructor(protected app: ApplicationContract) {}
 
   public register() {
     // Register your own bindings
+    /*
+    this.app.container.bind('App/Models/LocalCredential', () => LocalCredential)
+    this.app.container.bind('App/Models/User', () => User)
+    this.app.container.bind('App/Models/Password', () => Password)
+    this.app.container.bind('App/Model/PasswordCredential', () => PasswordCredential)
+    */
   }
 
   public async boot() {
