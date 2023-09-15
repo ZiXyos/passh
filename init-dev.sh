3b6b4128d7d139ba720bc6a61f8a80f00660f994
ZiXyos <killian.moudery@gmail.com>
2023-09-15 13:38:13 +0200

    fix(mono): astro and docker prblm

--- init-dev.sh
+++ init-dev.sh
@@ -2,6 +2,6 @@
 #
 mkcert -install
 mkcert "pass-ass.local" "*.pass-ass.local"
-mkdir -p docker/certs
-cp pass-ass.local+1-key.pem pass-ass.local+1.pem docker/certs
+mkdir -p .docker/certs
+cp pass-ass.local+1-key.pem pass-ass.local+1.pem .docker/certs
 rm -rf pass-ass.local+1*
