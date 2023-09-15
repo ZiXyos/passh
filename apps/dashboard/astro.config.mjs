d282928ebd89afa5fb0f350c3200fbc0db4ad2cb
ZiXyos <killian.moudery@gmail.com>
2023-09-15 20:36:34 +0200

    chore(dashboard): change from react to svelte

--- apps/dashboard/astro.config.mjs
+++ apps/dashboard/astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
+import svelte from '@astrojs/svelte'
 import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
 	compressHTML: true,
-	integrations: [mdx(), tailwind(), compress(), react()],
+	integrations: [mdx(), tailwind(), compress(), svelte()],
 	server: ({ command }) => ({ port: command === 'dev' ? 8000: 3434, host: true })
 });
